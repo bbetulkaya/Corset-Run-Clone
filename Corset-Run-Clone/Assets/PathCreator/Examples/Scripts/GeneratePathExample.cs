fd041fbdd55997e157465c6ccbb82e7435ec820d
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-12-17 11:32:59 +0300

    + Refactore : generate path in the editor with button

--- Corset-Run-Clone/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
+++ Corset-Run-Clone/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
+using NaughtyAttributes;
 
-namespace PathCreation.Examples {
+namespace PathCreation.Examples
+{
     // Example of creating a path at runtime from a set of points.
 
     // [RequireComponent(typeof(PathCreator))]
-    public class GeneratePathExample : MonoBehaviour {
+    public class GeneratePathExample : MonoBehaviour
+    {
 
         public bool closedLoop = true;
         public Transform[] waypoints;
 
-        void Start () {
-            if (waypoints.Length > 0) {
+        //Later, if you want to generate a path on run-time, copy and paste code on the below to Start function
+        [Button("Generate Path")]
+        public void GeneratePath()
+        {
+            if (waypoints.Length > 0)
+            {
                 // Create a new bezier path from the waypoints.
-                
-                BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
+
+                BezierPath bezierPath = new BezierPath(waypoints, closedLoop, PathSpace.xyz);
                 bezierPath.GlobalNormalsAngle = 90;
-                GetComponent<PathCreator> ().bezierPath = bezierPath;
+                GetComponent<PathCreator>().bezierPath = bezierPath;
             }
         }
     }
