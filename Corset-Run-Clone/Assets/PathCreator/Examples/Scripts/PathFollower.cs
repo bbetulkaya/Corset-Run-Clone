277cad7edfba44308b40618889eacc4b21c4b427
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-12-20 00:01:57 +0300

    + Refactore : refactore path follower script

--- Corset-Run-Clone/Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ Corset-Run-Clone/Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -6,7 +6,6 @@ namespace PathCreation.Examples
     // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
     public class PathFollower : MonoBehaviour
     {
-        public Transform visual;
         public PathCreator pathCreator;
         public EndOfPathInstruction endOfPathInstruction;
         public float speed = 5;
@@ -26,13 +25,8 @@ namespace PathCreation.Examples
             if (pathCreator != null)
             {
                 distanceTravelled += speed * Time.deltaTime;
-                visual.transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-                Vector3 tempPosition = visual.transform.position;
-                tempPosition.y = 2f;
-                visual.transform.position  = tempPosition;
-
-                // Fix the rotation!!!
-                visual.transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
             }
         }
 
@@ -40,7 +34,7 @@ namespace PathCreation.Examples
         // is as close as possible to its position on the old path
         void OnPathChanged()
         {
-            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(visual.transform.position);
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
         }
     }
 }
